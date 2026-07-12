cc52859993714c39bd9a2db6901217812d029e2d
cakewalk <cakewalk714@gmail.com>
2026-07-12 12:44:23 +0530

    refactor: add type-safe WebSocket message subscriptions

--- frontend/app/context/WebsocketContext.tsx
+++ frontend/app/context/WebsocketContext.tsx
@@ -4,36 +4,73 @@ import { createContext, useCallback, useContext, useRef } from "react"
 import { useWebsocket } from "@/hooks/useWebsocket";
 
 
-type WebSocketMessage =
 
-    | {
-        type: "code_change";
+
+type MessageMap = {
+    code_change: {
+        type: "code_change",
         code: string
-    }
-    | {
+    },
+
+    language_change: {
         type: "language_change";
         language: string;
         code: string;
-    }
-    | {
-        type: "output",
+    };
+
+    output: {
+        type: "output";
         output: string[];
-    }
+    };
+}
 
-type MessageHandler = (message: WebSocketMessage) => void
+// keyof MessageMap is "code_change" | "language_change" | "output"
+// so WebSocketMessage = {type: "code_change", code: string} | etc..
 
+type WebSocketMessage = MessageMap[keyof MessageMap]
+
+
+// T extends keyof MessageMap means T can only be one of the keys(code_change" | "language_change" | "output).
+// so T = "code_change" | T = "language_change" | T = "output"
+
+// so MessageMap[T] = MessageMap["output"] | MessageMap["language_change"] | MessageMap["code_change"]
+
+// so type MessageHandler<"output"> becomes (message: { type: "output"; output: string[]; }) => void
+
+type MessageHandler<T extends keyof MessageMap> = (message: MessageMap[T]) => void
+
+
+// subscribe(
+//     type: "output",
+//     handler: MessageHandler<"output">
+// )
+
+//expands to 
+
+
+// subscribe(
+//     type: "output",
+//     handler: (
+//         message: {
+//             type: "output";
+//             output: string[];
+//         }
+//     ) => void
+// )
 
 type WebsocketContextType = {
     isConnected: boolean,
     sendMessage: (message: WebSocketMessage) => void,
-    subscribe: (
-        type: WebSocketMessage["type"],
-        handler: MessageHandler
+    subscribe: <T extends keyof MessageMap> (
+        type: T,
+        handler: MessageHandler<T>
     ) => () => void
 }
 
 
 const WebsocketContext = createContext<WebsocketContextType | null>(null);
+
+
 
 
 export function WebsocketProvider({
@@ -46,7 +83,7 @@ export function WebsocketProvider({
 
 
     const listeners = useRef(
-        new Map<WebSocketMessage["type"], Set<MessageHandler>>()
+        new Map<keyof MessageMap, Set<(message: WebSocketMessage) => void>>()
     );
 
 
@@ -62,23 +99,24 @@ export function WebsocketProvider({
 
 
 
-    const subscribe = useCallback((type: WebSocketMessage["type"], handler: MessageHandler) => {
-        if (!listeners.current.get(type)) {
-            listeners.current.set(type, new Set())
-        }
-
-        listeners.current.get(type)?.add(handler);
-
-        //unsubscribe
-
-        return () => {
-            listeners.current.get(type)?.delete(handler);
-
-            if (listeners.current.get(type)?.size === 0) {
-                listeners.current.delete(type)
+    const subscribe = useCallback(
+        <T extends keyof MessageMap>(type: T, handler: MessageHandler<T>) => {
+            if (!listeners.current.get(type)) {
+                listeners.current.set(type, new Set())
             }
-        }
-    }, [])
+
+            listeners.current.get(type)?.add(handler as (message : WebSocketMessage) => void);
+
+            //unsubscribe
+
+            return () => {
+                listeners.current.get(type)?.delete(handler as ((message : WebSocketMessage) => void));
+
+                if (listeners.current.get(type)?.size === 0) {
+                    listeners.current.delete(type)
+                }
+            }
+        }, [])
 
 
 
